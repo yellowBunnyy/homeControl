c69e486cd692df3c9f6e1b0012fe586ea71653d3
yellow <yellowBunnyy@gmail.com>
2019-10-24 14:32:02 +0200

    add functionality including adding tokens to db and reseting single tokens

--- logic_script/dht_handler.py
+++ logic_script/dht_handler.py
@@ -49,8 +49,11 @@ class DHT_Handler(Container):
 	# errors = {'0': 'No sensor!! Check connection...', '1': 'Wrong read!!'}
 	sensor_errors_header = 'sensor_errors'
 
+
 	def __init__(self, data_path=p1_data, sensors_path=p2_sensors, errors_path=p3_errors_path, file_obj=obj_save_file):
-		super().__init__(data_path, sensors_path, errors_path, file_obj)	
+		super().__init__(data_path, sensors_path, errors_path, file_obj)
+		self.SQL_obj = save_to_file.HandlerFileSQL(db_file=self.db_errors_path)
+		self.table_name = 'errors_tokens'	
 
 
 	def dict_with_keys_as_room_names_and_dict_as_value(self) -> dict:
@@ -119,18 +122,32 @@ class DHT_Handler(Container):
 	def to_flask(self, pin:int, sensor_name:str, data_from_file:dict) -> dict:
 		'''method who create correct data form in allow range e.g 
 		{temp: readed < 100, humidity: 20 < readed < 95) and return that data'''
-
+		 
 		def remove_token_error(sensor_name:str):
 			'''This method remove all tokens from sensor when reads is OK'''			
 			# this variable represen how much we have tokens in sensor
 			self.file_obj.update_file(path=self.errors_path, key=sensor_name, content=0)
 			print(f'all tokens was remove from {sensor_name}')
+			###################### db ##################
+			self.SQL_obj.update_token_in_column(table_name=self.table_name, 
+												input_data={sensor_name:0})
+			###################### db ##################
+
 
 		def add_token_error(sensor_name:str, sensor_token_int:int):
 			''' This methon add one token to sensor when it's somthing wrong with reads '''
 			# this variable represen how much we have tokens in sensor
 			sensor_token_int += 1
-			self.file_obj.update_file(path=self.errors_path, key=sensor_name, content=sensor_token_int)
+			self.file_obj.update_file(path=self.errors_path,
+			 							key=sensor_name, 
+			 							content=sensor_token_int)
+			###################### db ##################
+			int_from_db = self.SQL_obj.fetch_token_int_from_column(
+										table_name=self.table_name, 
+										column_name=sensor_name)
+			self.SQL_obj.update_token_in_column(table_name=self.table_name,
+												input_data={sensor_name:int_from_db})
+			###################### db ##################
 			print(f'Token was added to {sensor_name} token info {sensor_token_int}!!')				
 			if sensor_token_int >=10:
 				print(f'błąd w {sensor_name}!!!!!!!')
@@ -158,13 +175,12 @@ class DHT_Handler(Container):
 			AND create new dict object called 'sensor_errors_header' (for keep tokens to shows errors) 
 			in main data file saved in .json '''
 		###################### db ##################
-		table_name = 'errors_tokens'
-		columns = [col for room_name, pin in self.names_container_default.items()]
-		SQL_obj = save_to_file.HandlerFileSQL(db_file=self.db_errors_path)
-		if SQL_obj.recognize_if_table_in_db_exist(table_name=table_name):
-			SQL_obj.create_table(table_name=table_name, columns=columns)
+		
+		columns = [col for room_name, pin in self.names_container_default.items()]		
+		if self.SQL_obj.recognize_if_table_in_db_exist(table_name=self.table_name):
+			self.SQL_obj.create_table(table_name=self.table_name, columns=columns)
 		# here we set all tokens on 0.
-			SQL_obj.update_token_in_column(table_name=table_name, data, reset_tokens=columns)
+			self.SQL_obj.update_token_in_column(table_name=self.table_name, data, reset_tokens=columns)
 		###################### db ##################
 		
 		# create_contener method automaticly check if file exist				
