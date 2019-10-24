f11c51de71d502a98c6e34ea09dcb6579df6db5d
yellow <yellowBunnyy@gmail.com>
2019-10-24 15:22:53 +0200

    fix some issue involve implement db file

--- logic_script/save_to_file.py
+++ logic_script/save_to_file.py
@@ -1,4 +1,4 @@
-import os, json, sys, datetime, csv
+import os, json, sys, datetime, csv, sqlite3
 
 os.chdir('/home/pi/Desktop/env/fl/src')
 print(os.getcwd())
@@ -214,104 +214,105 @@ class HandlerCsv(HandlerFile):
 
 class HandlerSQL(HandlerCsv):
 
-	def __init__(self, db_file:str=None):
-        '''Here we initial conection to database and create
-        cursor object form connect object.'''
-        # create connection with db and create cursor
-        if not db_file:
-            raise MyErrors('Please insert DataBase file path!!')
-        else:
-            self.db_file = db_file
-            self.conn = sqlite3.connect(db_file)
-            self.c = self.conn.cursor()
-            print('have connetction')
+	def __init__(self):
+		'''Here we initial conection to database and create
+		cursor object form connect object.'''
+		# create connection with db and create cursor
+		db_file = self.STATIC_DB_ERRORS_PATH
+		if not db_file:
+			raise MyErrors('Please insert DataBase file path!!')
+		else:
+			self.db_file = db_file
+			self.conn = sqlite3.connect(db_file)
+			self.c = self.conn.cursor()
+			print('have connetction')
 
-    def close_db(self):
-        # close db
-        self.conn.close()
+	def close_db(self):
+		# close db
+		self.conn.close()
 
-    def create_table(self, table_name:str, columns:(tuple,list)):
-        '''Create table in data base'''
-        # create new table
-        # print(','.join(name_col for name_col in columns))
-        self.c.execute('''CREATE TABLE {}
+	def create_table(self, table_name:str, columns:(tuple,list)):
+		'''Create table in data base'''
+		# create new table
+		# print(','.join(name_col for name_col in columns))
+		self.c.execute('''CREATE TABLE {}
 				   ({})'''.format(table_name, ','.join(name_col for name_col in columns)))
-        print('table was created --> {}'.format(table_name))
+		print('table was created --> {}'.format(table_name))
 
-    def save_data_to_db(self, data:tuple, table_name:str, *comumns):
-        # this condition create table in db if dose not exist
-        if self.recognize_if_table_in_db_exist(table_name=table_name):
-            self.create_table(table_name=table_name, columns=comumns)
+	def save_data_to_db(self, data:tuple, table_name:str, *comumns):
+		# this condition create table in db if dose not exist
+		if self.recognize_if_table_in_db_exist(table_name=table_name):
+			self.create_table(table_name=table_name, columns=comumns)
 
-        if type(data) != tuple:
-            raise MyErrors('wrong data format!! allowed only tuple!!')
-        else:
-            self.c.execute('''INSERT INTO {} VALUES ({})
+		if type(data) != tuple:
+			raise MyErrors('wrong data format!! allowed only tuple!!')
+		else:
+			self.c.execute('''INSERT INTO {} VALUES ({})
 				 '''.format(table_name, ','.join('?' * len(data))), data)
-            print('data was saved --> {}'.format(data))
-            self.conn.commit()
-            print('was commited')
-            self.close_db()
-            print('db was closed')
+			print('data was saved --> {}'.format(data))
+			self.conn.commit()
+			print('was commited')
+			self.close_db()
+			print('db was closed')
 
-    def compare_with_current_temp_and_save(self, full_time):
-        data = tuple(full_time.split(','))
-        temps_values_tup = tuple(value for room, value in self.TEMP_DATA.items())
-        all_data = data + temps_values_tup
-        print(all_data, '||||||')
-        if current_time in TRIGGER_HOURS:
-            self.save_data_to_db()
-            pass
+	def compare_with_current_temp_and_save(self, full_time):
+		data = tuple(full_time.split(','))
+		temps_values_tup = tuple(value for room, value in self.TEMP_DATA.items())
+		all_data = data + temps_values_tup
+		print(all_data, '||||||')
+		if current_time in TRIGGER_HOURS:
+			self.save_data_to_db()
+			pass
 
-    def read_from_db(self, table_name):
-        for row in self.c.execute('''SELECT * FROM {}'''.format(table_name)):
-            print(row)
+	def read_from_db(self, table_name):
+		for row in self.c.execute('''SELECT * FROM {}'''.format(table_name)):
+			print(row)
 
-    def create_random_input_data(self, no_col):
-        date, hour = datetime.datetime.now().strftime('%d-%m-%Y,%H:%M').split(',')
-        if no_col > 2:
-            t = tuple(random.choices(range(-20, 35), k=no_col - 2))
-            return (date, hour) + t
-        else:
-            raise MyErrors('not enough columns no_col > 2')
+	def create_random_input_data(self, no_col):
+		date, hour = datetime.datetime.now().strftime('%d-%m-%Y,%H:%M').split(',')
+		if no_col > 2:
+			t = tuple(random.choices(range(-20, 35), k=no_col - 2))
+			return (date, hour) + t
+		else:
+			raise MyErrors('not enough columns no_col > 2')
 
-    def recognize_if_table_in_db_exist(self, table_name:str) -> bool:
-        '''Recognizon if table is in data base. If is return false
-            otherwise return true'''
-        # script search table which is input --> (table_name) arg this metchod
-        self.c.execute('''SELECT name FROM sqlite_master WHERE type="table" AND name="{}"'''.format(table_name))
-        if self.c.fetchone():
-            print('db exist --> flag :false')
-            return False
-        else:
-            print('db does not exist --> flag: true')
-            return True
+	def recognize_if_table_in_db_exist(self, table_name:str) -> bool:
+		'''Recognizon if table is in data base. If is return false
+			otherwise return true'''
+		# script search table which is input --> (table_name) arg this metchod
+		self.c.execute('''SELECT name FROM sqlite_master WHERE type="table" AND name="{}"'''.format(table_name))
+		if self.c.fetchone():
+			print('db exist --> flag :false')
+			return False
+		else:
+			print('db does not exist --> flag: true')
+			return True
 
-    def update_token_in_column(self, table_name:str, input_data:(dict, bool) = False, reset_all_tokens:list = False):
-        '''Update value in single column in table, key as room name val as token int 
-        e.g ('salon': 1) are a dictionary'''
-        # reset all tokens in columns       
-        if reset_all_tokens and data == False:
-        	for column in reset_all_tokens:
-        		print(table_name, column, 0)
-        		self.c.execute('''UPDATE {} SET {} = {}'''.format(
-                                                        table_name,
-                                                        column,
-                                                        0))
-        #set token in column
-        else:
-        	return_key_or_value_form_dict = lambda dic_t, value=None: list(dic_t.values())[-1] if value else list(dic_t.keys())[-1]        	
-	        print(table_name, return_key_or_value_form_dict(dic_t=input_data), return_key_or_value_form_dict(dic_t=data,value=True))
-	        self.c.execute('''UPDATE {} SET {} = {}'''.format(
-	                                                        table_name,
-	                                                        return_key_or_value_form_dict(dic_t=input_data),
-	                                                        return_key_or_value_form_dict(dic_t=input_data, value=True)) + 1) # here we update old value add 1.
-	        self.conn.commit()
+	def update_token_in_column(self, table_name:str, input_data:(dict, bool) = False, reset_all_tokens:list = False):
+		'''Update value in single column in table, key as room name val as token int 
+		e.g ('salon': 1) are a dictionary'''
+		# reset all tokens in columns       
+		if reset_all_tokens and data == False:
+			for column in reset_all_tokens:
+				print(table_name, column, 0)
+				self.c.execute('''UPDATE {} SET {} = {}'''.format(
+														table_name,
+														column,
+														0))
+		#set token in column
+		else:
+			return_key_or_value_form_dict = lambda dic_t, value=None: list(dic_t.values())[-1] if value else list(dic_t.keys())[-1]        	
+			print(table_name, return_key_or_value_form_dict(dic_t=input_data), return_key_or_value_form_dict(dic_t=data,value=True))
+			self.c.execute('''UPDATE {} SET {} = {}'''.format(
+															table_name,
+															return_key_or_value_form_dict(dic_t=input_data),
+															return_key_or_value_form_dict(dic_t=input_data, value=True)) + 1) # here we update old value add 1.
+			self.conn.commit()
 
-    def fetch_token_int_from_column(self, table_name:str, column_name:str) -> int:
-        '''fetch token as integer from data base and return that integer'''
-        db_data = self.c.execute('''SELECT {} FROM {}'''.format(column_name, table_name))
-        return db_data.fetchone()[-1]
+	def fetch_token_int_from_column(self, table_name:str, column_name:str) -> int:
+		'''fetch token as integer from data base and return that integer'''
+		db_data = self.c.execute('''SELECT {} FROM {}'''.format(column_name, table_name))
+		return db_data.fetchone()[-1]
 
 
 	
