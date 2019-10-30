b71828b377f96b0c8506e6a015a33319447f58da
yellow <yellowBunnyy@gmail.com>
2019-10-30 13:36:49 +0100

    delete all usles code responsible to handle (save, update) in .json file

--- logic_script/dht_handler.py
+++ logic_script/dht_handler.py
@@ -124,47 +124,34 @@ class DHT_Handler(Container):
 		 
 		def remove_token_error(sensor_name:str):
 			'''This method remove all tokens from sensor when reads is OK'''			
-			# this variable represen how much we have tokens in sensor
-			self.file_obj.update_file(path=self.errors_path, key=sensor_name, content=0)
-			print(f'all tokens was remove from {sensor_name}')
-			###################### db ##################
 			self.SQL_obj.update_token_in_column(table_name=self.table_name, 
 												input_data={sensor_name:0})
-			###################### db ##################
+			print(f'all tokens was remove from {sensor_name}')			
 
 
-		def add_token_error(sensor_name:str, sensor_token_int:int):
+		def add_token_error(sensor_name:str):
 			''' This methon add one token to sensor when it's somthing wrong with reads '''
-			# this variable represen how much we have tokens in sensor
-			sensor_token_int += 1
-			self.file_obj.update_file(path=self.errors_path,
-										key=sensor_name, 
-										content=sensor_token_int)
-			###################### db ##################
-			int_from_db = self.SQL_obj.fetch_token_int_from_column(
+
+			# this -> int_token_from_db variable represen how much we have tokens in sensor
+			int_token_from_db = self.SQL_obj.fetch_token_int_from_column(
 										table_name=self.table_name, 
 										column_name=sensor_name) + 1			
 			self.SQL_obj.update_token_in_column(table_name=self.table_name,
-												input_data={sensor_name:int_from_db})
-			print(int_from_db, sensor_name, 10*'|')
-			###################### db ##################
-			print(f'Token was added to {sensor_name} token info {sensor_token_int}!!')				
-			if sensor_token_int >=10:
-				print(f'błąd w {sensor_name}!!!!!!!')
-				return f'{sensor_name} tu mamy błąd'
+												input_data={sensor_name:int_token_from_db})			
+			print(f'Token was added to {sensor_name} token info {int_token_from_db}!!')				
+			if int_token_from_db >=10:
+				print(f'błąd w {sensor_name}!!!!!!!')				
 
 		readed_data = self.recognicion_device(pin=pin, name=sensor_name)
 
-		if not readed_data or readed_data == 10:
-			sensor_token_int = self.file_obj.load_from_json(path=self.errors_path)[sensor_name]
-			add_token_error(sensor_name=sensor_name, sensor_token_int=sensor_token_int)
+		if not readed_data or readed_data == 10:			
+			add_token_error(sensor_name=sensor_name)
 			print('from file!!!', data_from_file[sensor_name])
 			return data_from_file[sensor_name]				
 		else:									
 			data = {name: round(value,1) if value != None else value for name, value in zip(['temp','humidity'], readed_data[::-1])}				
 			self.file_obj.update_file(path=self.data_path, key='temps', key2=sensor_name, content=data)
-			remove_token_error(sensor_name=sensor_name)
-			# print('from sensor', data)			
+			remove_token_error(sensor_name=sensor_name)						
 			return data
 	
 
@@ -174,20 +161,14 @@ class DHT_Handler(Container):
 			e.g {'salon': {'temp':20, 'humidity'}, 'maly_pokoj': {'temp':20, 'humidity'}, itd.}
 			AND create new dict object called 'sensor_errors_header' (for keep tokens to shows errors) 
 			in main data file saved in .json '''
-		###################### db ##################
-		
+			
 		columns = [col_room_name for col_room_name, pin in self.names_container_default.items()]		
 		if self.SQL_obj.recognize_if_table_in_db_exist(table_name=self.table_name):
 			self.SQL_obj.create_table(table_name=self.table_name, columns=columns)
 		# here we set all tokens on 0.
 			self.SQL_obj.save_data_to_db(data=tuple([0 for _ in range(len(columns))]),
 												table_name=self.table_name)
-
-		###################### db ##################
 		
-		# create_contener method automaticly check if file exist				
-		self.file_obj.create_container(path=self.errors_path, 
-			content={name: 0 for name in self.names_container_default})			
 		container = self.dict_with_keys_as_room_names_and_dict_as_value()
 		return container
 
