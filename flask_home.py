fb47994a7b9a2a75ceddb3bfe3cfb84813d9f8c4
yellow <yellowBunnyy@gmail.com>
2019-11-29 15:02:49 +0100

    moved to .db from .json saved data without temperatures from sensors

--- flask_home.py
+++ flask_home.py
@@ -9,7 +9,6 @@ TEMP_KEY = 'temps'
 SOCKETS_TABLE = 'sockets_table'
 SENSOR_ERRORS = 'sensor_errors'
 save_to_file_obj = save_to_file.HandlerFile()
-virtual_relay_obj = virtual_relay.Relays_class(obj=save_to_file_obj)
 DATA_PATH = save_to_file_obj.STATIC_PATH
 SENSOR_PATH = save_to_file_obj.STATIC_SENSOR_PATH
 LIGHTING_PATH = save_to_file_obj.STATIC_LIGHTING_PATH
@@ -20,7 +19,8 @@ dht_handler_obj = dht_handler.DHT_Handler(
 		errors_path = ERRORS_PATH,		
 		file_obj=save_to_file_obj)
 SQL_obj = dht_handler_obj.SQL_obj
-
+virtual_relay_obj = virtual_relay.Relays_class(obj=save_to_file_obj, 
+												SQL_obj=SQL_obj)
 #create container to data
 print(save_to_file_obj.create_container(DATA_PATH))
 
@@ -78,63 +78,50 @@ def set_time_request_handling():
 	if it is a POST methode othewise send to site response.
 	We use here dht_handler_obj instead save_to_file_obj because there we
 	have variable which have reference to HandlerSQL. So is useless to create new
-	reference.'''
-	# Check if table exist.
-	# True if does not existance otherwise False	
-	if SQL_obj.recognize_if_table_in_db_exist(table_name=SOCKETS_TABLE):
-			# here we create table
-			SQL_obj.create_table(table_name=SOCKETS_TABLE, 
-								columns=('ON','OFF'))
-			# add default hours values to row when we crate a new table
-			SQL_obj.save_data_to_db(table_name=SOCKETS_TABLE,
-									data=('00:00','00:00'))
+	reference.'''	
 	# POST
 	if request.method == 'POST':
 		# fetch data from site and decode to dict object		
-		data = json.loads(request.data)
-		# save data to file
-		save_to_file_obj.update_file(path=DATA_PATH, key='sockets', content=data)
-		###########db###########
-		for key, val in data.items():
-			SQL_obj.update_token_in_column(table_name=SOCKETS_TABLE,
-											input_data={key:val})
-		###########db###########
+		data = json.loads(request.data)			
+		# times in string ON and Off received from site
+		tup_data = tuple(data.values())		
+		SQL_obj.update_data_in_sockets_table(times=tup_data, row=1)		
 		return 'OK'
 	# GET
-	else:       
-		data = json.dumps(save_to_file_obj.load_from_json(path=DATA_PATH,key='sockets'))
-		###########db###########
-		db_data = {}
-		# here we got column names form tabel. In this case 'ON' or 'OFF'
-		columns_list = SQL_obj.fetch_column_names(table_name=SOCKETS_TABLE)
-		# in this case 'ON' or 'OFF'
-		for single_col in columns_list:			
-			str_hour = SQL_obj.fetch_token_int_from_column(table_name=SOCKETS_TABLE,
-															column_name=single_col)
-			db_data[single_col] = str_hour			
-		print(db_data,'|')
-		###########db###########
-		return data
+	else:		
+		fetched_times_tuple = SQL_obj.fetch_all_data_from_sockets(row=1)
+		data_to_send = dict(zip(['ON','OFF'], fetched_times_tuple))		
+		db_data = json.dumps(data_to_send)		
+		return db_data
 
 @app.route('/settimeHeat', methods=['POST', 'GET'])
 def set_time_heat():        
-	if request.method == 'POST':		                
-		data = json.loads(request.data)		
-		save_to_file_obj.update_file(DATA_PATH, 'heat_switch', data)
+	if request.method == 'POST':
+		# times recived from site converted to dict.		                
+		data = json.loads(request.data)			
+		SQL_obj.update_data_in_sockets_table(times=tuple(data.values()),
+											row=2) # heaters		
 		return 'ok'
-	else:				
-		data = save_to_file_obj.load_from_json(DATA_PATH, 'heat_switch')		
-		return json.dumps(data)
+	else:		
+		fetched_data = SQL_obj.fetch_all_data_from_sockets(row=2) # heaters switch		
+		data_from_db = dict(zip(['ON','OFF'], fetched_data))					
+		return json.dumps(data_from_db)
 
 @app.route('/settempHeat', methods=['POST', 'GET'])
 def set_temp_heat():
 	if request.method == 'POST':
-		data = json.loads(request.data)		
-		save_to_file_obj.update_file(DATA_PATH, 'heats', data)
+		data = json.loads(request.data)				
+		# with wc and outside but values are setet to 0 
+		tokens_int = tuple(data.values()) + (0,0,)
+		SQL_obj.update_data_tokens(temperature_int=tokens_int, row=2)# heaters temperature set		
 		return 'ok'
-	else:		
-		data = save_to_file_obj.load_from_json(DATA_PATH, 'heats')		        
-		return json.dumps(data)
+	else:
+		to_remove = ["WC", "outside"]		
+		data_db = SQL_obj.fetch_data_from_tokens(row=2, show_dict=True)		
+		# dict with rooms
+		data_to_send = {room: val for room, val in data_db.items() 
+						if room not in to_remove}				        
+		return json.dumps(data_to_send)
 
 
 @app.route('/current', methods=['POST', 'GET'])
