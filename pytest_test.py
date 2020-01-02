03a1f0879900c9f668751cd304655b06098f94d1
yellow <yellowBunnyy@gmail.com>
2020-01-02 09:50:48 +0100

    add next tests

--- pytest_test.py
+++ pytest_test.py
@@ -1,15 +1,28 @@
 from logic_script import dht_handler, save_to_file
 room_names = save_to_file.HandlerFile().room_names
-import pytest, flask_home
+import pytest, flask_home, os
+
+test_path = os.path.join(os.getcwd(),'logic_script','test_csv.csv')
 
 @pytest.fixture(name='SQL_obj')
 def SQL_obj_class():
 	SQL_obj = save_to_file.HandlerSQL()
 	return SQL_obj
-@pytest.fixture(name='csv_obj')
+
+@pytest.fixture(name='csv_in_class')
 def csv_cls():
 	csv_obj = save_to_file.HandlerCsv()
 	return csv_obj
+
+@pytest.fixture(name='pure_csv')
+def pure_csv(backend):
+	csv_obj = save_to_file.CSV_Class(backend_file=backend.save_to_file())
+	return csv_obj
+
+@pytest.fixture(name='backend')
+def create_backend():
+	backend_obj = save_to_file.FileHandler(path=test_path) 
+	return backend_obj
 
 ## SQL
 	## tbl temperature_humidity
@@ -69,8 +82,38 @@ def test_names_and_pins_correct_output_data(SQL_obj):
 
 
 ##CSV
-def test_saved_data_to_csv(csv_obj):
-	pass
+@pytest.mark.parametrize('time_examples',(
+	(['23-10-2019,08:00', '24-10-2019,10:00', '25-10-2019,14:00']),
+	(['30-10-2019,01:00', '31-10-2019,22:00', '31-10-2019,11:00']),
+	))	
+def test_saved_data_to_csv_main_file(csv_in_class, backend, time_examples):
+# create and save data to file 	
+	triggers = csv_in_class.TRIGGER_HOURS
+	for full_time in time_examples:
+		csv_in_class.save_temp_to_csv_handler(path=test_path, full_time=full_time)
+		f = backend.save_to_file()
+		_, expected = save_to_file.CSV_Class(
+				backend_file=f).read_csv(close_file=True)			
+	else:
+		f = backend.save_to_file()
+	_, rows = save_to_file.CSV_Class(
+		backend_file=f).read_csv(close_file=True)
+	assert expected == rows
+
+
+def test_save_data_to_csv_main_file_correct_data_last_row(csv_in_class, pure_csv, backend):
+# check if last row saved row are valid. then delete file.
+	data = dict(zip(room_names, range(len(room_names))))
+	date, hour = '31-10-2019,22:00'.split(',')
+	expected = {'date':date, 'hour':hour}
+	expected.update(data)	
+	csv_in_class.save_to_file_csv(path=test_path, data=data, hour=hour, date=date)			
+	fetched_data, _ = pure_csv.read_csv(close_file=True)
+	last_row = fetched_data[-1]
+	assert expected == last_row
+	backend.delete_file()
+
+	
 
 
 
