cf56100be803723e45a53bd33c7e0a8704e52cf5
yellow <yellowBunnyy@gmail.com>
2020-02-06 09:42:37 +0100

    add new tests

--- pytest_test.py
+++ pytest_test.py
@@ -1,6 +1,6 @@
-from logic_script import dht_handler, save_to_file
+from logic_script import dht_handler, save_to_file, virtual_relay
 room_names = save_to_file.HandlerFile().room_names
-import pytest, flask_home, os, Adafruit_DHT
+import pytest, flask_home, os, Adafruit_DHT, flask, json, random
 from unittest.mock import patch
 from unittest.mock import Mock
 
@@ -19,6 +19,7 @@ def SQL_obj_class_not_called():
 	obj = save_to_file.HandlerSQL
 	return obj
 
+# csv fixture
 @pytest.fixture(name='csv_in_class')
 def csv_cls():
 	csv_obj = save_to_file.HandlerCsv()
@@ -42,6 +43,54 @@ def create_backend():
 def dht_class_obj():
 	dht_obj = dht_handler.DHT_Handler()
 	return dht_obj
+
+@pytest.fixture(name='dht_obj_not_called')
+def dht_cls_obj():
+	obj = dht_handler.DHT_Handler
+	return obj
+
+@pytest.fixture(name='simpleyTestingCls')
+def simpleytTestingCls():
+	obj = dht_handler.SimpleyTesting()
+	return obj
+
+# virtual relay fixture
+
+@pytest.fixture(name='obj_v_relay_not_called')
+def obj_virtual_realay():
+	obj = virtual_relay.Relays_class
+	return obj
+
+# save to file fixture
+@pytest.fixture(name='handler_file_obj')
+def save_to_file_obj():
+	obj = save_to_file.HandlerFile()
+	return obj
+
+@pytest.fixture(name='handler_file_not_called')
+def save_to_file_obj_not_called():
+	obj = save_to_file.HandlerFile
+	return obj
+
+# create file 
+@pytest.fixture(name='test_json_file')
+def create_test_file():
+	path = r'/home/pi/Desktop/env/fl/src/logic_script/test.json'
+	content = {'salon':1, 'wc':2}
+	with open(path, 'w') as f:
+		f.write(json.dumps(content))
+	yield path, content 
+	os.remove(path)
+
+
+	
+
+#monkeypath
+@pytest.fixture(name='no_req')
+def no_request(monkeypath):
+	monkeypath.delattr('flask.request.method')
+	
+
 
 ## SQL
 	## tbl temperature_humidity
@@ -239,14 +288,6 @@ def test_with_mock_ret_str(input_data, expected, dht_obj):
 		from_method = dht_obj.recognicion_device(pin=pin, name=name)
 		assert from_method == expected
 
-# @pytest.mark
-# def test_with_mock_raise_err_pin_incorrect_pin(dht_obj):
-# 	pin, name = 34, 'unknow'
-# 	# przechwytuje exception		
-# 	with pytest.raises(Exception):
-# 		from_method = dht_obj.recognicion_device(pin=pin, name=name)
-
-		
 
 @pytest.mark.parametrize('input_data, expected', (
 	((7, 'salon'), tuple),
@@ -307,18 +348,292 @@ def test_with_mock_return_correct_data(input_data, expected, dht_obj):
 
 # Remove token Error
 
-# def test_with_mock_remove_token(sql_obj_not_called, dht_obj):
-# 	sql_obj_not_called.fetch_data_from_tokens = Mock()
-# 	sql_obj_not_called.fetch_data_from_tokens.return_value = 'do nothing'
+@pytest.mark.parametrize('input_data', (
+	({'salon':0, 'WC':0,'outside':0}),
+	({'salon':-30, 'WC':2,'outside':23}),
+	({'salon':None, 'WC':0,'outside':0}),
+	({'outside':0}),
+	({'salon':None, 'WC':'cokolwiek'}),
+	({}),
+	))
+def test_with_mock_remove_token(input_data, sql_obj_not_called, dht_obj):
+	expected = tuple(input_data.values())
+	sql_obj_not_called.fetch_data_from_tokens = Mock()
+	sql_obj_not_called.fetch_data_from_tokens.return_value = input_data
+	sql_obj_not_called.update_data_tokens = Mock()
+	sql_obj_not_called.update_data_tokens.return_value = 'do nothing'
+	from_method = dht_obj.remove_token_error(sensor_name='')
+	assert from_method == expected
+	sql_obj_not_called.fetch_data_from_tokens.assert_called()
+	sql_obj_not_called.update_data_tokens.assert_called()
+
+# add token_error
+@pytest.mark.parametrize('input_data, expected',(
+	(({'salon':0, 'WC':0,'outside':0}, 'WC'), (0,1,0)),
+	(({'salon':9, 'WC':0,'outside':0}, 'salon'), (10,0,0)),
+	(({}, 'salon'),()),
+	(({'salon':'stri', 'WC':0,'outside':0}, 'salon'), (None,0,0)),
+	(({'salon':2, 'WC':0,'outside':0}, 'everythig'), (2,0,0)),
+	))
+def test_with_mock_add_token(input_data, expected, sql_obj_not_called, dht_obj):
+	dict_data, sensor_name = input_data
+	sql_obj_not_called.fetch_data_from_tokens = Mock()
+	sql_obj_not_called.fetch_data_from_tokens.return_value = dict_data
+	sql_obj_not_called.update_data_tokens = Mock()
+	sql_obj_not_called.update_data_tokens.return_value = 'do nothing'
+	from_method = dht_obj.add_token_error(sensor_name=sensor_name)
+	assert from_method == expected
+	sql_obj_not_called.fetch_data_from_tokens.assert_called()
+	sql_obj_not_called.update_data_tokens()
+
+# update
+
+def test_update(sql_obj_not_called, dht_obj):
+	input_data = {'salon': {'temp':20, 'humidity':40}, 'maly_pokoj': {'temp':20, 'humidity':60}}
+	expected = input_data	
+	dht_obj.dict_with_keys_as_room_names_and_dict_as_value = Mock()
+	dht_obj.dict_with_keys_as_room_names_and_dict_as_value.return_value= input_data
+	sql_obj_not_called.update_data_in_temperature = Mock()
+	sql_obj_not_called.update_data_in_temperature.return_value = 'do nothing'
+	from_method = dht_obj.update()
+	assert from_method == expected	
+	dht_obj.dict_with_keys_as_room_names_and_dict_as_value.assert_called()
+	sql_obj_not_called.update_data_in_temperature.assert_called()
+
+
+
+###### CLASS SimpleyTesting
+
+# single sensor
+
+@pytest.mark.parametrize('input_data', (
+	((11, 7, 'salon')),
+	((22, 4, 'outside')),
+	))
+def test_single_sensor_correct_data(input_data, simpleyTestingCls):
+	sensor_name, pin, room_name = input_data
+	from_method = simpleyTestingCls.single_sensor(
+		sensor=sensor_name, pin=pin, name=room_name)
+
+@pytest.mark.parametrize('input_data', (
+	((12, 7, 'salon')), # wrong sensor test
+	((11, 41,'salon')), # wrong pin > 31	
+	))
+def test_single_sensor_raise_err(input_data, simpleyTestingCls):
+	sensor, pin, name = input_data
+	with pytest.raises(Exception):
+		from_method = simpleyTestingCls.single_sensor(sensor=sensor, pin=pin,name=name)	
+
+# all sensors in row
+
+def test_all_sensors_in_row(SQL_obj, simpleyTestingCls):
+	input_dict = SQL_obj.names_and_pins_default
+	from_method = simpleyTestingCls.all_sensors_in_row(sensors_dict=input_dict)
+
+
+##### FLASK HOME (Main file)
+
+# temperature backgound
+@pytest.mark.parametrize('input_data', (
+	({'salon':{'temp':34,'humidity':60}, 
+		'WC': {'temp':24,'humidity':29},
+		'outside':{'temp':-2,'humidity':98}}),
+	))
+def test_temp_bg(input_data, sql_obj_not_called):
+	expected = json.dumps(input_data)
+	sql_obj_not_called.fetch_all_data_from_temp = Mock()
+	sql_obj_not_called.fetch_all_data_from_temp.return_value = input_data
+	from_method = flask_home.temp_background()
+	assert from_method == expected
+	sql_obj_not_called.fetch_all_data_from_temp.assert_called()
+
+@pytest.mark.parametrize('input_data', (
+	([1,2,3,4]),
+	({1,2,3,4}),
+	('some_string'),
+	(23),
+	))
+def test_temp_bg_raise_err(input_data, sql_obj_not_called):
+	sql_obj_not_called.fetch_all_data_from_temp = Mock()
+	sql_obj_not_called.fetch_all_data_from_temp.return_value = input_data	
+	with pytest.raises(Exception):
+		from_method = flask_home.temp_background()
+	sql_obj_not_called.fetch_all_data_from_temp.assert_called()
+
+@pytest.mark.parametrize('input_data', (
+	({'salon':{'temp':34,'humidity':60}, 
+		'WC': {'temp':24,'humidity':29},
+		'outside':{'temp':-2,'humidity':98}}),
+	))
+def test_temp_return_correct_data(input_data, sql_obj_not_called, dht_obj_not_called):
+	expected = json.dumps(input_data)
+	sql_obj_not_called.fetch_all_data_from_temp = Mock()
+	sql_obj_not_called.fetch_all_data_from_temp.return_value = ''
+	dht_obj_not_called.dict_with_keys_as_room_names_and_dict_as_value = Mock()
+	dht_obj_not_called.dict_with_keys_as_room_names_and_dict_as_value.return_value = input_data
+	from_method = flask_home.temp_background()
+	assert from_method == expected
+	dht_obj_not_called.dict_with_keys_as_room_names_and_dict_as_value.assert_called()
+	sql_obj_not_called.fetch_all_data_from_temp.assert_called()
+
+
+# lighting relays loader
+@patch.object(virtual_relay.Relays_class, 'lighting_handler', return_value="don't call function")
+def test_lighting_relays_call(mock_v_r):
+	from_method = flask_home.lighting_relays_loader()
+	assert from_method
+
+# set time request handling
+
+def test_set_time():
+	# próba za pomocą patch.mock.Mock()
+	flask.request = Mock()
+
+	# from_method = flask_home.
+	# mocked_f.headers.get.assert_called()
+
+
+##### SAVE TO FILE MODULE
+
+## Handler File Class
+
+# create container
+
+@pytest.mark.parametrize('input_data, expected', (
+	((r'/home/pi/Desktop/env/fl/src/logic_script', False), ValueError),
+	((r'/home/pi/Desktop/env/fl/src/logic_script/test.txt', False), 'container was created!!'),
+	((r'/home/pi/Desktop/env/fl/src/logic_script/test.txt', True), 'container exist!!'),
+	))
+def test_create_container(input_data, expected,  handler_file_obj):
+	path, bool_val = input_data
+	mock_f = os.path
+	mock_f.isfile = Mock(return_value=bool_val)
+	if type(expected) == str:
+		from_method = handler_file_obj.create_container(path=path)
+		if os.path.isfile(path):
+			os.remove(path)
+		assert from_method
+	else:
+		with pytest.raises(Exception):
+			from_method = handler_file_obj.create_container(path=path)		
+	
+# save to json
+
+def test_save_to_json(handler_file_obj):
+	path = r'/home/pi/Desktop/env/fl/src/logic_script/test.txt'
+	content = {'somthing': 'lol'}
+	from_method = handler_file_obj.save_to_json(path=path, content=content)
+	assert from_method
+	if os.path.isfile(path):
+		os.remove(path)
+
+
+# load from json
+@pytest.mark.parametrize('input_key', (
+	(None),		
+	('salon'),
+	))
+def test_load_from_json(input_key, test_json_file, handler_file_obj):
+	path, content = test_json_file		
+	from_method = handler_file_obj.load_from_json(path=path, key=input_key)
+	if not input_key :
+		assert from_method == content
+	else:
+		assert from_method == content[input_key]
+
+
+@pytest.mark.parametrize('input_data, content', (
+	([r'/home/pi/Desktop/env/fl/src/logic_script/test.json', None], ''),
+	([r'/home/pi/Desktop/env/fl/src/logic_script/test.json', 'blabla'],
+		{'salon':1, 'wc':2}),
+	([r'/home/pi/Desktop/env/fl/src/logic_script/test.json', 'salon'],
+		'common string'),
+	([r'/home/pi/Desktop/env/fl/src/logic_script/test.json', 'blabla'],
+		'{"blaca"}'),
+	([r'/home/pi/Desktop/env/fl/src/logic_script/test.json', 'blabla'],
+		'{blaca}'),
+	))
+def test_load_from_json_raise_err(input_data, content, handler_file_obj):
+	path, key = input_data
+	def create_file(path, content):
+		with open(path, 'w') as f:
+			content = json.dumps(content)
+			f.write(content)		
+	create_file(path=path, content=content)
+	with pytest.raises(Exception):
+		from_method = handler_file_obj.load_from_json(path=path, key=key)
+	os.remove(path)
+
+# update file
+@pytest.mark.parametrize('input_data', (
+	(['/home/pi/Desktop/env/fl/src',{'salon': 12, 'maly_pokoj':123, 'WC': 900},'WC','changed value', None]),
+	(['/home/pi/Desktop/env/fl/src',{'salon': 12, 'maly_pokoj':123, 'WC': {'temp': 20, 'humidity': 99}},'WC','changed value', 'temp']),
+	))
+def test_update_file(input_data, handler_file_obj, handler_file_not_called):
+	path, mocked_content, key, swap_content, key2 = input_data
+	handler_file_not_called.load_from_json = Mock()
+	handler_file_not_called.load_from_json.return_value = mocked_content
+	handler_file_not_called.save_to_json = Mock()
+	handler_file_not_called.save_to_json.return_value = 'do nothing'
+	from_method = handler_file_obj.update_file(path=path, 
+		key=key, content=swap_content, key2=key2)
+	assert from_method == swap_content		
+	handler_file_not_called.save_to_json.assert_called()		
+	handler_file_not_called.load_from_json.assert_called()
+	
+
+@pytest.mark.parametrize('input_data', (
+	(['/home/pi/Desktop/env/fl/src',{'salon': 12, 'maly_pokoj':123, 'WC': 900}, 'bla', 'changed value', None]),
+	(['/home/pi/fake/dir',{'salon': 12, 'maly_pokoj':123, 'WC': 900}, 'bla', 'changed value', None]),
+	(['/home/pi/Desktop/env/fl/src', {'salon': 12, 'maly_pokoj':123, 'WC': 900}, 'salon', '', None]),
+	))
+def test_update_file_raise_err(input_data, handler_file_obj, handler_file_not_called):
+	path, mocked_content, key, swap_content, key2 = input_data
+	handler_file_not_called.load_from_json = Mock()
+	handler_file_not_called.load_from_json.return_value = mocked_content
+	handler_file_not_called.save_to_json = Mock()
+	handler_file_not_called.save_to_json.return_value = 'do nothing'
+	with pytest.raises(Exception):
+		from_method = handler_file_obj.update_file(path=path, key=key, content=swap_content, key2=key2)
+	
+
+# delete data from file
+
+@pytest.mark.parametrize('input_data', (
+	(['/home/pi/Desktop/env/fl/src', 'salon', {'salon':23, 'WC':34, 'outside': -1}]),
+	))
+def test_delete_data_from_file(input_data, handler_file_not_called, handler_file_obj):
+	path, key, fake_content = input_data		
+	handler_file_not_called.load_from_json = Mock()
+	handler_file_not_called.load_from_json.return_value = fake_content
+	handler_file_not_called.save_to_json = Mock()
+	handler_file_not_called.save_to_json.return_value = 'do nothing'	
+	from_method = handler_file_obj.delete_data_from_file(path=path, key=key)	
+	assert from_method == fake_content
+	handler_file_not_called.load_from_json.assert_called()
+	handler_file_not_called.save_to_json.assert_called()
+
+
+@pytest.mark.parametrize('input_data', (
+	(['/home/pi/Desktop/env/fl/src', 'fakeKey', {'salon':23, 'WC':34, 'outside': -1}]),
+	(['/fake/directory', 'WC', {'salon':23, 'WC':34, 'outside': -1}]),
+	))
+def test_delete_data_raise_err(input_data, handler_file_not_called, handler_file_obj):
+	path, key, fake_content = input_data
+	handler_file_not_called.load_from_json = Mock()
+	handler_file_not_called.load_from_json.return_value = fake_content
+	handler_file_not_called.save_to_json = Mock()
+	handler_file_not_called.save_to_json.return_value = 'do nothing'
+	with pytest.raises(Exception):
+		from_method = handler_file_obj.delete_data_from_file(path=path, key=key)
+		handler_file_not_called.load_from_json.assert_called()
+		handler_file_not_called.save_to_json.assert_called()
+
+
+
+
+
 	
 	
-
-
-
-
-
-
 	
-
-
 
